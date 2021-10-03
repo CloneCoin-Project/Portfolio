912ed1ff0c96bca8c6aae68c6ce2b29e05016f6d
gimseung-ug <rlatmd0829@naver.com>
2021-10-03 17:29:29 +0900

    코인 조회 get -> post로 변경

--- src/main/java/com/cloneCoin/portfolio/controller/CoinController.java
+++ src/main/java/com/cloneCoin/portfolio/controller/CoinController.java
@@ -4,7 +4,7 @@ import com.cloneCoin.portfolio.dto.CoinRequestDto;
 import com.cloneCoin.portfolio.dto.CoinResponseDto;
 import com.cloneCoin.portfolio.service.CoinService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +18,7 @@ public class CoinController {
 
     private final CoinService coinService;
 
-    @GetMapping("/coin")
+    @PostMapping("/coin")
     public List<CoinResponseDto> coinReturn(@RequestBody CoinRequestDto coinRequestDto){
         return coinService.coinReturn(coinRequestDto);
 
